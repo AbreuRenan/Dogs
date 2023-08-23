94c1b988d5ca4a5b91fb71aa194e4a289c421aa2
AbreuRenan <renan.de.abreu@gmail.com>
2023-08-23 16:34:54 -0300

    useForm e validacao básica finalizada

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-function Input({ label, type, name, placeholder, erroMsg }) {
+function Input({ label, type, name, value, onChange, onBlur, error }) {
   return (
     <div className={styles.inputWrapper}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      <input className={styles.input} type={type} name={name} />
-      <p className={styles.inputError}>{erroMsg}</p>
+      <input
+        className={styles.input}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.inputError}>{error}</p>}
     </div>
   );
 }
