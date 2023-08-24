37564d05d055a123b167a5c1aa858b900adf0e06
AbreuRenan <renan.de.abreu@gmail.com>
2023-08-24 20:44:09 -0300

    estilizado login e criado breakpoint para mobile

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -7,6 +7,8 @@ import Recuperarconta from "./Recuperarconta";
 import Resetarsenha from "./Resetarsenha";
 import { UserContext } from "../../UserContext";
 
+import styles from "./Login.module.css";
+
 function Login() {
   const { logedIn, loading } = useContext(UserContext);
   if (logedIn === true) return <Navigate to="/conta" />;
@@ -15,14 +17,16 @@ function Login() {
       {loading ? (
         "Carregando..."
       ) : (
-        <div>
-          <Routes>
-            <Route path="/" element={<LoginForm />} />
-            <Route path="criar" element={<Criarconta />} />
-            <Route path="perdeu" element={<Recuperarconta />} />
-            <Route path="reset" element={<Resetarsenha />} />
-          </Routes>
-        </div>
+        <section className={styles.login}>
+          <div className={`${styles.forms} animeLeft`}>
+            <Routes>
+              <Route path="/" element={<LoginForm />} />
+              <Route path="criar" element={<Criarconta />} />
+              <Route path="perdeu" element={<Recuperarconta />} />
+              <Route path="reset" element={<Resetarsenha />} />
+            </Routes>
+          </div>
+        </section>
       )}
     </>
   );
