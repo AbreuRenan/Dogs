dfa05494580d7d17820c020070d9ea4ce3d640f3
AbreuRenan <renan.de.abreu@gmail.com>
2023-08-28 21:40:47 -0300

    corrigido label login

--- src/Components/Auth/LoginForm.jsx
+++ src/Components/Auth/LoginForm.jsx
@@ -25,8 +25,8 @@ function LoginForm() {
     <section className="animeLeft">
       <h1 className="title">Login</h1>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <Input label="usuario" type="text" name="username" {...username} />
-        <Input label="usuario" type="password" name="password" {...password} />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         {loading ? (
           <Button disabled>Carregando</Button>
         ) : (
